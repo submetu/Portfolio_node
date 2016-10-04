637f3ba21176d1a62dce5468e1a60c9d2aa66faa
M Subhan Naeem <sub.metu@gmail.com>
2016-10-04 16:11:07 +0300

    Added the nav partial and included it in the layout.jade file, also updated construction messages for routes

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/about',function(req,res,next){
+	res.send("About page under construction");
+});
+router.get('/contact',function(req,res,next){
+	res.send("Contact page under construction");
+});
 
 module.exports = router;
