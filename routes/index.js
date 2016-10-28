998a36b745237907004f71a5eea577fbbd3b2408
M Subhan Naeem <sub.metu@gmail.com>
2016-10-29 01:10:35 +0300

    Added full email functionality with error handling

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var nconf = require('nconf');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +16,30 @@ router.get('/contact',function(req,res,next){
 });
 router.get('/photos',function(req,res,next){
 	res.render('photos', { title: 'Express', path: req.path });
+});
+router.post('/emailPost',function(req,res,next){
+	var user_name = nconf.get('user_name');
+	var password = nconf.get('password');
+	var smtpTransport = nodemailer.createTransport("SMTP",{
+	    service: "Gmail",
+	    auth: {
+	        user: user_name,
+	        pass: password
+	    }
+	});
+	var mailOptions = {
+		to : 'sub.metu@gmail.com',
+		subject : req.body.email,
+		text : req.body.message
+	}
+    smtpTransport.sendMail(mailOptions, function(error, response){
+	     if(error){
+	         res.sendStatus(400);
+	     }
+	     else{
+	        res.sendStatus(200);
+	     }
+	});
 });
 
 module.exports = router;
